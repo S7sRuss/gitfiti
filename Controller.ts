822571ff93dead043bb0bb327ad25c271eba0bde
José Torres <jftorres29@gmail.com>
2019-08-27 19:45:29 +0100

    Update Controller.ts for 2019-08-27

--- Controller.ts
+++ Controller.ts
@@ -10,3 +10,5 @@
 // TODO: Implement Controller functionality
 // Updated on 2019-06-15
 // TODO: Implement Controller functionality
+// Updated on 2019-08-27
+// TODO: Implement Controller functionality
